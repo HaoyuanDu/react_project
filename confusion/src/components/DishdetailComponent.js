77d4fef4cdc2395e76e8939f6bec4e101adef81d
HaoyuanDu <jasonduuuu@hotmail.com>
2020-09-06 15:18:50 -0400

    assignment 2
    Integrated the AboutComponent given above into the single page application.
    Added a new functional component named <RenderLeader> through the RenderLeader() function to AboutComponent.js that renders the details of a given leader using the reactstrap <Media> component.
    Construct and render the list of leaders in the About Us page using the <RenderLeader> component implemented above.

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -20,28 +20,15 @@ import { Link } from 'react-router-dom';
             )
         })
         return (
-            <div className="container">
-            <div className="row">
-                <Breadcrumb>
+            
+            <div className='col-12 col-md-5 m-1'>
+                <h4> Comments </h4>
+                <ul className='list-unstyled'>
+                    {cmnts}
+                </ul>
 
-                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>{props.dish.name}</h3>
-                    <hr />
-                </div>                
             </div>
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <RenderDish dish={props.dish} />
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    <RenderComments comments={props.comments} />
-                </div>
-            </div>
-            </div>
-        );
+        )
     }
 
     function RenderDish({dish}) {
@@ -72,13 +59,27 @@ import { Link } from 'react-router-dom';
         }
         return (
             <div className="container">
-                <div className="row">
+            <div className="row">
+                <Breadcrumb>
+
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
                     <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.dish.comments} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
-            
-        )
+            </div>
+        );
     }
 
 
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,57 +1,62 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
-
-
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES,
-        comments: COMMENTS,
-        promotions: PROMOTIONS,
-        leaders: LEADERS
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,  // lifting the state up
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
+        };
+    }
 
-  render() {
-    const HomePage = () => {
-        return(
-            <Home 
-                dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-                leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-            />
+    render() {
+        const HomePage = () => {
+            return(
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
+            );
+          }
+
+        const DishWithId = ({match}) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.id === parseInt(match.params.dishId,10))}
+                />
+            );
+        }
+
+        return (
+            <div>
+                <Header />
+                    <Switch>
+                        <Route path='/home' component={HomePage} />
+                        <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                        <Route path="/menu/:dishID" component={DishWithId} />
+                        <Route exact path="/contactus" component={Contact} />
+                        <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
+                        <Redirect to="/home" />
+                    </Switch>
+                <Footer />
+            </div>
         );
-      }
-    return (
-      <div>
-        {/* <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar> */}
-        <Header />
-        <Switch>
-            <Route path="/home" component={HomePage} />
-            <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
-            <Route exact path='/contactus' component={Contact} />
-            <Redirect to="/home" />        
-        </Switch>
-        <Footer />
-      </div>
-    );
-  }
+    }
 }
 
 export default Main;
