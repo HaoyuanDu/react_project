824767720c36d7bd75a7252c14c21c53217b9720
HaoyuanDu <jasonduuuu@hotmail.com>
2020-10-08 13:26:16 -0400

    fixed dishdetail

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -21,21 +21,21 @@ import {Card, CardImg, CardText, BreadcrumbItem , Breadcrumb,
 
     function RenderComments ({comments}){
         if (comments != null )
-        return (
-            <div className="col-12 col-md-5 m-1">
-                <h4>Comments</h4>
-                <ul className="list-unstyled">
-                    {comments.map((comment) => {
-                        return(
-                        <li key={comment.id}>
-                        <p>{comment.comment}</p>
-                        <p>-- {comment.author}</p>
-                        </li>
-                        )
-                    })}
-                </ul>
-            </div>
-        );
+            return (
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {comments.map((comment) => {
+                            return(
+                            <li key={comment.id}>
+                            <p>{comment.comment}</p>
+                            <p>-- {comment.author}</p>
+                            </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            );
         else return(
             <div><h1>ye comment ka area ha</h1></div>
         )
@@ -43,28 +43,28 @@ import {Card, CardImg, CardText, BreadcrumbItem , Breadcrumb,
 
 const DishDetail = (props) => {
    if(props.dish != null)
-   return (
-       <div className="conatiner">
-               <div className="row m-2">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to="/home">Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>{props.dish.name}</h3>
-                    <hr/>
+        return (
+            <div className="conatiner">
+                    <div className="row m-2">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/home">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr/>
+                        </div>
+                        </div>
+                <div className="row">
+                <RenderDish dish={props.dish}/>
+                <RenderComments comments={props.comments}/>
                 </div>
-                </div>
-        <div className="row">
-        <RenderDish dish={props.dish}/>
-        <RenderComments comments={props.comments}/>
-        </div>
-       </div>
-   );
+            </div>
+        );
    else 
-   return(
-       <div><h1>ye pic ka area ha</h1></div>
-   );
+        return(
+            <div><h1>ye pic ka area ha</h1></div>
+        );
 }
 
 
